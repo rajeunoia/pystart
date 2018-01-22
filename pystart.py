5d84bb041bc5252842cf3a846c6ccfb1efada8e9
Raja <rajsekhar.cs@gmail.com>
2018-01-22 17:20:23 +0530

    Converted terminal program into cli mode

--- pystart.py
+++ pystart.py
@@ -12,13 +12,12 @@ def support():
 
 
     msg = ""
-    # Get a response to the input text 'How are you?'
-    while msg.upper() != "BYE":
-        msg = input("You: ")
-        response = chatbot.get_response(msg)
-        if(response.confidence > 0.5 and msg.upper() != "BYE"):
-            print(response)
-        elif(msg.upper() != "BYE"):
-            print("Sorry Didn't get you? Try these questiosn")
-            sample_questions = ["Will i get a job after training?", "When is the next workshop?", "What is taught in 3 days workshop?"]
-            print(sample_questions)
+    reply = ""
+    response = chatbot.get_response(msg)
+    if(response.confidence > 0.5 and msg.upper() != "BYE"):
+        reply = response
+    elif(msg.upper() != "BYE"):
+        reply = "Sorry Didn't get you? Try these questions <br>"
+        sample_questions = ["Will i get a job after training?", "When is the next workshop?", "What is taught in 3 days workshop?"]
+        reply += sample_questions
+    return reply
