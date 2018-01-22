5f6ffd812c21ee06b100991973d1ae45186eec03
Raja <rajsekhar.cs@gmail.com>
2018-01-22 21:02:06 +0530

    statement conversion to dictonary

--- pystart.py
+++ pystart.py
@@ -12,9 +12,11 @@ def support(msg):
     reply = ""
     response = chatbot.get_response(msg)
     if(response.confidence > 0.5 and msg.upper() != "BYE"):
-        reply = response
+        reply = response.serialize()
     elif(msg.upper() != "BYE"):
         reply = "Sorry Didn't get you? Try these questions <br>"
         sample_questions = ["Will i get a job after training?", "When is the next workshop?", "What is taught in 3 days workshop?"]
         reply += sample_questions
+    else:
+        reply = "Thanks for talking to me, see you soon."
     return reply
