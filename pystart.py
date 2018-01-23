e419c7339699fc1e830d18ecbda4e4bbef702525
Raja <rajsekhar.cs@gmail.com>
2018-01-23 09:41:06 +0530

    small errors are missing.

--- pystart.py
+++ pystart.py
@@ -12,6 +12,25 @@ def support():
         'Pyson',
         trainer='chatterbot.trainers.ListTrainer'
     )
+    msg = "Hi"
+    reply = ""
+    response = chatbot.get_response(msg)
+    if(response.confidence > 0.5 and msg.upper() != "BYE"):
+        reply = response.serialize()
+    elif(msg.upper() != "BYE"):
+        reply = "Sorry Didn't get you? Try these questions <br>"
+        sample_questions = ["Will i get a job after training?", "When is the next workshop?", "What is taught in 3 days workshop?"]
+        reply += sample_questions
+    else:
+        reply = "Thanks for talking to me, see you soon."
+    return json.dumps(reply)
+
+@app.route('/support/<msg>',methods = ['POST', 'GET'])
+def support(msg):
+    chatbot = ChatBot(
+        'Pyson',
+        trainer='chatterbot.trainers.ListTrainer'
+    )
     #msg = "Hi"
     reply = ""
     response = chatbot.get_response(msg)
