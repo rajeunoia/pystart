63171bbbc4367c0934137ce770114ca9a806ca18
Raja <rajsekhar.cs@gmail.com>
2018-02-16 21:46:01 +0530

    table exists issue renamed

--- connects/facebook.py
+++ connects/facebook.py
@@ -2,7 +2,7 @@ import json
 import sqlite3 as sq
 
 
-def table_exists(conn,table_name):
+def admin_table_exists(conn,table_name):
     result = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+table_name+"'")
     result_data = result.fetchall()
     if len(result_data)>0 and result_data[0][0] == table_name:
@@ -12,7 +12,7 @@ def table_exists(conn,table_name):
 
 def insert_page(page_data):
     conn = sq.connect("bot_admin.db")
-    if table_exists(conn,"connected_pages"):
+    if admin_table_exists(conn,"connected_pages"):
         conn.execute('insert into connected_pages("user_id","page_id","page_name","page_token") values ("'+page_data["userid"]+'","'+page_data["pageid"]+'","'+page_data["pagename"]+'","'+page_data["pagetoken"]+'")')
     else:
         conn.execute('create table  connected_pages ("id" INTEGER PRIMARY KEY AUTOINCREMENT,"user_id" CHAR(30) NOT NULL,"page_id" CHAR(30), "page_name" CHAR(30), "page_token" CHAR(200))')
